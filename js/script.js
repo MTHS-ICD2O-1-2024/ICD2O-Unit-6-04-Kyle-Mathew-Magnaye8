42fe6ea6a08db64379eb199acd56a9faf6973288
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-05-22 16:51:57 +0000

    Added code in 6-04

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Kyle Matthew Magnaye
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kyle Matthew Magnaye
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculateVolume() {
+  const radius = parseFloat(document.getElementById("radius-of-a-sphere").value)
+
+  if (isNaN(radius)) {
+    document.getElementById("user-info").innerText =
+      "Please enter a valid number."
+    return
+  }
+
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3)
+  document.getElementById("user-info").innerText =
+    "Volume: " + volume.toFixed(2) + " cm³"
+}
